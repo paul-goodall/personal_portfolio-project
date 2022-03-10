93e8137c79a90340e8a46d3178795b1937431e78
paul-goodall <paul.thomas.goodall@gmail.com>
2022-03-10 16:58:20 +1100

    new edits

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,3 +10,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class NavbarObject(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
